48a7fb951815367ee883594a3721b99850efa785
Mike Gentry <mgentry007@gmail.com>
2017-06-25 17:06:08 -0700

    employee info design

--- js/app.js
+++ js/app.js
@@ -2,9 +2,30 @@ $.ajax({
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
     success: function(data) {
+        // $.each(data.items, function (i, results) {
+
+
+        let empObject = data.results[0];
+        let photo = empObject.picture.large;
+        let firstName = empObject.name.first;
+        let lastName = empObject.name.last;
+        let email = empObject.email;
+        let city = empObject.location.city;
         let profileHTML = '<tr>';
-        profileHTML += '<td><img src=' + data.results[0].picture.thumbnail + '>';
+        profileHTML += '<td><img class=photo src=' + photo + '>';
+        profileHTML += '<ul><li>' + firstName +' '+ lastName + '</li>';
+        profileHTML += '<li>' +email + '</li>';
+        profileHTML += '<li>' + city + '</li>';
+        profileHTML += '</li></ul></td>';
+        profileHTML += '</tr>';
         $('table').html(profileHTML);
+
+        // });
         console.log(data);
     }
 });
+
+
+
+
+
