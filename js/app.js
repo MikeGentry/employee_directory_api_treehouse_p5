54645997ccefea8f6742fba770b74fc02b6dbb58
Mike Gentry <mgentry007@gmail.com>
2017-06-25 18:15:34 -0700

    employee info design

--- js/app.js
+++ js/app.js
@@ -2,25 +2,21 @@ $.ajax({
     url: 'https://randomuser.me/api/?results=12',
     dataType: 'json',
     success: function(data) {
-        // $.each(data.items, function (i, results) {
-
-
-        let empObject = data.results[0];
-        let photo = empObject.picture.large;
-        let firstName = empObject.name.first;
-        let lastName = empObject.name.last;
-        let email = empObject.email;
-        let city = empObject.location.city;
-        let profileHTML = '<tr>';
-        profileHTML += '<td><img class=photo src=' + photo + '>';
-        profileHTML += '<ul><li>' + firstName +' '+ lastName + '</li>';
-        profileHTML += '<li>' +email + '</li>';
-        profileHTML += '<li>' + city + '</li>';
-        profileHTML += '</li></ul></td>';
+        let profileHTML = '';
+        let empCount = 0;
+        profileHTML = '<tr>';
+        $.each(data.results, function (i, emp) {
+            if (empCount < 3) {
+                profileHTML += '<td><img class=photo src=' + emp.picture.large + '>';
+                profileHTML += '<ul><li>' + emp.name.first + ' ' + emp.name.last + '</li>';
+                profileHTML += '<li>' + emp.email + '</li>';
+                profileHTML += '<li>' + emp.location.city + '</li>';
+                profileHTML += '</li></ul></td>';
+                $('table').html(profileHTML);
+                empCount++;
+            }
+        });
         profileHTML += '</tr>';
-        $('table').html(profileHTML);
-
-        // });
         console.log(data);
     }
 });
