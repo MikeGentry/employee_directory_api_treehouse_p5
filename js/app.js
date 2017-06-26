bb60b43d4ec23b8156982f0716fa031e6a6efdd6
Mike Gentry <mgentry007@gmail.com>
2017-06-25 21:12:55 -0700

    replaced table with grid layout

--- js/app.js
+++ js/app.js
@@ -3,20 +3,16 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
         let profileHTML = '';
-        let empCount = 0;
-        profileHTML = '<tr>';
         $.each(data.results, function (i, emp) {
-            if (empCount < 3) {
-                profileHTML += '<td><img class=photo src=' + emp.picture.large + '>';
-                profileHTML += '<ul><li>' + emp.name.first + ' ' + emp.name.last + '</li>';
-                profileHTML += '<li>' + emp.email + '</li>';
-                profileHTML += '<li>' + emp.location.city + '</li>';
-                profileHTML += '</li></ul></td>';
-                $('table').html(profileHTML);
-                empCount++;
-            }
+            profileHTML += '<div class=employee>'
+            profileHTML += '<img class=photo src=' + emp.picture.large + '>';
+            profileHTML += '<ul><li>' + emp.name.first + ' ' + emp.name.last + '</li>';
+            profileHTML += '<li>' + emp.email + '</li>';
+            profileHTML += '<li>' + emp.location.city + '</li>';
+            profileHTML += '</li></ul>';
+            profileHTML += '</div>';
+            $('.container').html(profileHTML);
         });
-        profileHTML += '</tr>';
         console.log(data);
     }
 });
