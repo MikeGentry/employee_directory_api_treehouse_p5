7ab8fa1ac2d614a49621dd904cbdd85e417ca5dd
Mike Gentry <mgentry007@gmail.com>
2017-06-29 18:00:40 -0700

    refactoring

--- js/app.js
+++ js/app.js
@@ -20,8 +20,7 @@ $.ajax({
             modalHTML += '<p>' + emp.email + '</p>';
             modalHTML += '<p>' + emp.cell + '</p>';
             modalHTML += '<p>' + emp.location.street + '</p>';
-            modalHTML += '<p>' + emp.location.city + ', ' + emp.location.state + '</p>';
-            modalHTML += '<p>' + emp.location.postcode + '</p>';
+            modalHTML += '<p>' + emp.location.city + ', ' + emp.location.state + ' ' + emp.location.postcode + '</p>';
             modalHTML += '<p>DOB: ' + emp.dob + '</p>';
             modalHTML += '</div>';
             j++;
@@ -31,14 +30,22 @@ $.ajax({
         console.log(data);
         let modalDiv = $('.modal').children('div');
         $('.employee').click(function (e) {
-            e.preventDefault();
             let target = e.currentTarget.id;
             for (let i = 0; i < modalDiv.length; i++) {
-                console.log(target, modalDiv[i].id);
                 if (target === modalDiv[i].id) {
                     $(function () {
                         $('.modal').html(modalDiv[i]).dialog({
+                            width: 350,
+                            dialogClass: 'no-close',
                             modal: true,
+                            buttons: [
+                                {
+                                    text: 'Close',
+                                    click: function () {
+                                        $(this).dialog('close');
+                                    }
+                                }
+                            ]
                         })
                     });
                 }
